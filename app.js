f956d4b99da7ff92500670c1d504be7201370715
Vamshix57 <vamshi.krishna@kalvium.community>
2024-04-24 12:12:12 +0530

    done

--- app.js
+++ app.js
@@ -1,142 +1,71 @@
-var cartValue = document.getElementById("cart-value");
-var cartButton = document.getElementById("cart");
+var btn = document.getElementsByClassName("cart");
+var Items = document.getElementById("cart-icon");
+var value = document.getElementById("cart-value");
 
-var addButtons = document.getElementsByClassName("button");
-
-var items = [
-  {
-    name: "This was our pact",
-    quantity: 0,
-    dollars: 7,
-    cents: 49,
-  },
-  {
-    name: "The famous five",
-    quantity: 0,
-    dollars: 4,
-    cents: 59,
-  },
-  {
-    name: "Matilda",
-    quantity: 0,
-    dollars: 6,
-    cents: 80,
-  },
-  {
-    name: "Harry Potter",
-    quantity: 0,
-    dollars: 10,
-    cents: 0,
-  },
-  {
-    name: "For Young Readers",
-    quantity: 0,
-    dollars: 7,
-    cents: 29,
-  },
-  {
-    name: "Wimpy Kid - DIY",
-    quantity: 0,
-    dollars: 4,
-    cents: 99,
-  },
-  {
-    name: "Dart Board",
-    quantity: 0,
-    dollars: 17,
-    cents: 49,
-  },
-  {
-    name: "Connect Four",
-    quantity: 0,
-    dollars: 19,
-    cents: 99,
-  },
-  {
-    name: "Jenga",
-    quantity: 0,
-    dollars: 20,
-    cents: 99,
-  },
-  {
-    name: "Monopoly",
-    quantity: 0,
-    dollars: 19,
-    cents: 49,
-  },
-  {
-    name: "Bookmarks",
-    quantity: 0,
-    dollars: 12,
-    cents: 49,
-  },
-  {
-    name: "Birthday Card",
-    quantity: 0,
-    dollars: 12,
-    cents: 49,
-  },
-  {
-    name: "Stuffed toys",
-    quantity: 0,
-    dollars: 15,
-    cents: 99,
-  },
-  {
-    name: "Dream catcher drawing",
-    quantity: 0,
-    dollars: 18,
-    cents: 49,
-  },
+list = [
+  { name: "This was our pact", quantity: 0, dollars: 7, cents: 49 },
+  { name: "The famous five", quantity: 0, dollars: 4, cents: 59 },
+  { name: "Matilda", quantity: 0, dollars: 6, cents: 80 },
+  { name: "Harry Potter", quantity: 0, dollars: 10, cents: 0 },
+  { name: "For Young Readers", quantity: 0, dollars: 7, cents: 29 },
+  { name: "Wimpy Kid - DIY", quantity: 0, dollars: 4, cents: 99 },
+  { name: "Dart Board", quantity: 0, dollars: 17, cents: 49 },
+  { name: "Connect Four", quantity: 0, dollars: 19, cents: 99 },
+  { name: "Jenga", quantity: 0, dollars: 20, cents: 99 },
+  { name: "Monopoly", quantity: 0, dollars: 19, cents: 49 },
+  { name: "Bookmarks", quantity: 0, dollars: 12, cents: 49 },
+  { name: "Birthday Card", quantity: 0, dollars: 12, cents: 49 },
+  { name: "Stuffed toys", quantity: 0, dollars: 15, cents: 99 },
+  { name: "Dream catcher drawing", quantity: 0, dollars: 18, cents: 49 },
 ];
 
-function updateCart() {
-  let cart = 0;
-  for (index = 0; index < items.length; index++) {
-    cart = cart + items[index].quantity;
+function Cart() {
+  let Items = 0;
+  for (i = 0; i < list.length; i++) {
+    Items += list[i].quantity;
   }
-  cartValue.innerHTML = cart;
+  value.innerHTML = cart;
 }
 
-for (let i = 0; i < addButtons.length; i++) {
-  addButtons[i].onclick = (e) => {
-    items[i].quantity++;
-    updateCart();
-  };
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", myFunction);
+  function myFunction() {
+    list[i].quantity++;
+    Cart();
+  }
 }
 
-var finalDollars = 0;
-var finalCents = 0;
-
-function updatePrice() {
+function Price() {
   let totalPriceInCents = 0;
 
-  for (index = 0; index < items.length; index++) {
+  for (i = 0; i < list.length; i++) {
     totalPriceInCents =
       totalPriceInCents +
-      items[index].quantity * (items[index].dollars * 100 + items[index].cents);
+      list[i].quantity * (list[i].dollars * 100 + list[i].cents);
   }
   finalDollars = Math.floor(totalPriceInCents / 100);
   finalCents = totalPriceInCents % 100;
 }
 
+Items.addEventListener("click", finalOutput);
 
-cartButton.onclick = () => {
-  updatePrice();
+function finalOutput() {
+  Price();
+  text = "";
 
-
-  for (let index = 0; index < items.length; index++) {
-    if (items[index].quantity != 0) {
+  for (var i = 0; i < list.length; i++) {
+    if (list[i].quantity !== 0) {
       console.log(
-        "Item name: " +
-          items[index].name +
-          " - Quantity: " +
-          items[index].quantity
+        "Item name: " + list[i].name + " - Quantity: " + list[i].quantity
       );
+      text += "Item name: " + list[i].name + " - Quantity: " + list[i].quantity;
     }
   }
-
   console.log(
     "The total amount is " + finalDollars + "$ and " + finalCents + " cents"
   );
-};
+
+  text +=
+    "The total amount is " + finalDollars + "$ and " + finalCents + " cents";
+  window.open("https://wa.me/9963858498?text=" + text);
+}
